4d2616af3202ec08280d5940611d2bd50fd74eab
Richie <richardokoye2@gmail.com>
2019-03-30 14:40:08 +0100

    add reset feature

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,41 +2,25 @@ import React, {Component} from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-	state = {
-		counters: [
-           {id: 1, value: 4},
-           {id: 2, value: 0},
-           {id: 3, value: 0},
-           {id: 4, value: 0}
-		]
-	}
 
-	  handleDelete = (counterId) => {
-    	const counters = this.state.counters.filter(m => m.id !== counterId);
-    	this.setState({counters}) 
- 	  };
- 	  handleReset = () => {
- 	  	const counters = this.state.counters.map(c => {
- 	  		c.value = 0;
- 	  		return c;
- 	  	});
- 	  	this.setState({counters});
- 	  }
 	render() {
+		const {onReset, onDelete, onIncrement, onDecrement, counters } = this.props;
 		return(
-			<React.Fragment>
+			<div>
 			<button 
-				onClick={this.handleReset}
-				className="btn btn-primary btn-sm m2">Reset
+					className="btn btn-primary btn-sm m-2"
+					onClick={onReset}>Reset
 			</button>
-			  {this.state.counters.map(counter => (
-			  	 <Counter 
-			  	 	key={counter.id} 
-			  	 	onDelete={() => this.handleDelete(counter.id)} 
-			  	 	counter={counter} >
-			  	 </Counter>
-			  	 ))}
-			</React.Fragment>
+			 	{this.props.counters.map(counter => (
+			 		<Counter 
+			 			key={counter.id} 
+			 			onDelete={onDelete}
+			 			onIncrement={onIncrement}
+			 			onDecrement={onDecrement}
+			 			counter={counter}
+			 	 />
+			 	))}
+			</div>
 			)
 	}
 }
