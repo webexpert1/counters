49fd99b2a82b06d14fd52c8cd616146e4ced2453
Richie <richardokoye2@gmail.com>
2019-04-01 23:25:18 +0100

    disable when less than 0

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,19 +14,25 @@ class Counter extends Component {
  	render() {
     
 	  return (
-        <div>
+        <div className="row">
+        <div className="col-1">
           <span className={this.formatBadge()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
           <button
              onClick={() => this.props.onIncrement(this.props.counter)} 
-             className="btn btn-info btn-sm m-2">Increment
+             className="btn btn-info btn-sm"> +
           </button>
           <button 
             onClick={() => this.props.onDecrement(this.props.counter)}
-            className="btn btn-secondary btn-sm">Decrement
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? 'disabled': ''}> -
           </button>
-          <button className="btn btn-danger m-2" 
+          <button className="btn btn-danger" 
                   onClick={() => this.props.onDelete(this.props.counter.id)}>Delete
           </button>
+        </div>
+    
         </div>
       );
   }
